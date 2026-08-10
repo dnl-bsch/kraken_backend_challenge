cfac3d8c9ca2e3e76690829b4663313361220b5e
Daniel Busch <daniel.busch@climate-resource.com>
2026-08-10 10:41:52 +0200

    admin page

--- importer/admin.py
+++ importer/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+from django.contrib import admin 
 
-# Register your models here.
+from importer.models import MeterReading
+
+@admin.register(MeterReading) 
+class MeterReadingAdmin(admin.ModelAdmin):
+    list_display = (  
+        "mpan", 
+        "meter_serial_number",  
+        "register_id", 
+        "reading_datetime",  
+        "reading_value",  
+    )  
+    search_help_text = "Search by MPAN or meter serial number"  
+    search_fields = (  
+        "meter__meter_point__mpan",  
+        "meter__serial_number", 
+    ) 
+    list_filter = ("register_id", "reading_datetime")  
+    date_hierarchy = "reading_datetime"  
+    ordering = ("-reading_datetime",) 
+    list_select_related = ("meter", "meter__meter_point")  
+
+    @admin.display(ordering="meter__meter_point__mpan", description="MPAN")  
+    def mpan(self, obj):  
+        return obj.meter.meter_point.mpan  
+
+    @admin.display(ordering="meter__serial_number", description="Meter Serial")  
+    def meter_serial_number(self, obj): 
+        return obj.meter.serial_number  
